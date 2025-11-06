aaad4f16f0837ffd3546457c1ac1af73a595c1ac
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2025-11-06 15:00:04 -0300

    repetir-texto.py

---
+++ repetir-texto.py
@@ -0,0 +1,14 @@
+# repetir_texto.py
+
+# Solicita uma string ao usuário
+texto = input("Digite uma palavra ou frase: ")
+
+# Solicita um número inteiro
+vezes = int(input("Quantas vezes deseja repetir? "))
+
+# Repete a string o número de vezes informado
+resultado = texto * vezes
+
+# Exibe o resultado
+print("Resultado da repetição:")
+print(resultado)
