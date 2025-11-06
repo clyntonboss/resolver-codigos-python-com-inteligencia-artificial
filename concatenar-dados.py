bc6ffe1aeb5b13e67ba30814aaa9092ac1214c30
Rogério Clynton Ribeiro <clynton.boss@gmail.com>
2025-11-06 14:58:58 -0300

    concatenar-dados.py

---
+++ concatenar-dados.py
@@ -0,0 +1,11 @@
+# concatenar_dados.py
+
+# Solicita dois dados ao usuário
+primeiro_dado = input("Digite o primeiro dado: ")
+segundo_dado = input("Digite o segundo dado: ")
+
+# Concatena os dados em uma única string
+resultado = primeiro_dado + " " + segundo_dado
+
+# Exibe o resultado
+print("Dados concatenados:", resultado)
